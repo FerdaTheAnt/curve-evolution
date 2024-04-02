9a4f9341ea7242448f52af5a18c4f75a50d39b47
FerdaTheAnt <petr.filip01@gmail.com>
2024-04-02 17:09:06 +0200

    Curve flow with force is now just a subtype of normal curve flow.

--- include/CurveFlowForce.h
+++ include/CurveFlowForce.h
@@ -1,25 +1,13 @@
 #pragma once
 
-#include "ODEProblem.h"
+#include "CurveFlow.h"
 
-class CurveFlowForce : public ODEProblem
+class CurveFlowForce : public CurveFlow
 {
     public:
         CurveFlowForce(int dimension, int n);
-        int getDegreesOfFreedom();
-        void setInitialCondition(double* u, void (*func)(double*, double));
-        virtual void getRightHandSide(const double& t, double* u, double* fu);
-        bool writeSolution(const double &t, int step, const double *u);
+        virtual void getRightHandSide(const double& t, double* u, double* fu) override;
         virtual ~CurveFlowForce();
-    protected:
-        int dimension;
-        int n;
-        double h;
-        double* d;
-        
-        //void getBackDifference(const double* u);
-        void getPartialLength(const double* u);
-        //void getLocalLength();
     private:
         //void (*force)(double*, double, double);
         double* F;
